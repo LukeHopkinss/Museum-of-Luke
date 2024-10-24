97111d1d674897617578e7708ca645f349321e69
Luke Hopkins <luke.hopkins@outlook.com>
2024-10-23 23:49:50 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Museum-of-Luke
+ This is just my portfolio, but it expands upon my other interests as well!
---
+++ src/constant.js
@@ -0,0 +1,61 @@
+export const scaleFactor = 4;
+
+export const dialogueData = {
+
+    exit: "Welcome to the Museum of Luke! Interact with different objects to learn more about me!",
+
+    stove: "This is where I cook. I'm still learning how to cook, so I don't have anything here to showcase unfortunately :(",
+
+    pots: "My best friend Roberto loves to make scultpures and he made these pots for me! I use them every single day! Go check out his work <a href='https://www.instagram.com/czystudio.us/'>here</a>!",
+
+    fridge2: "The only drinks in this fridge are water and coffee. Every since COVID I only drink water and coffee. During that time something in my brain switched and started only drinking water. As for coffee, I only recently started enjoying it, but <a href='https://drive.google.com/drive/folders/1RfQLXWMI547LD8geK8GnAdkP8g9uQ4k7?usp=drive_link'>here</a> are some of pictures of my indulgences!",
+
+    browntree1: "This is just a tree. I strive to decorate my environments with plants. I really believe that they make you a happier person.",
+
+    browntree2: "This is just another tree. I strive to decorate my environments with plants. I really believe that they make you a happier person.",
+
+    grandpa: "My grandpa here is getting ready to cook some pho. Because I am half Vietnamese, I grew up eating pho. It's one of my favorite dishes and I truly am blessed to have such caring grandparents. <a href='https://drive.google.com/drive/folders/1BrHw7jVUjDdhq2psXsVLpdACa5zDKv_N?usp=drive_link'>Here</a> are some pictures of my trip to Vietnam in 2019!",
+
+    table: "I do most of my journaling here. My older brother got me a journal while he was in Florence and ever since then I've been writing in it. I really recommend it to everyone because it has helped me understand my emotions on a multitude of new levels. <a href='https://drive.google.com/drive/folders/1_qo0_q1aXNsk3ZLTjD7i8PGc9aLXeNn1?usp=sharing'>Here</a> are some of my favorite entries!",
+
+    bunny: "I just thought this bunny was cute so I added him here. I have no pets, but I love all animals. If I was an animal I would want to be a penguin. ",
+
+    couch1: "I love to relax on couches and watch Youtube. Right now my favorite Youtuber is definitely <a href='https://www.youtube.com/@HorsesOnYT'>Horses</a>. I love their content and I think they have some of the most interesting and well-crafted videos on the site.",
+
+    couch2: "I love to relax on couches and watch Youtube. Right now my favorite Youtuber is definitely <a href='https://www.youtube.com/@HorsesOnYT'>Horses</a>. I love their content and I think they have some of the most interesting and well-crafted videos on the site.",
+
+    tablelamp: "Here is one of the creations my friends and I have made. We really enjoy the creative process and have been working on a lot of projects. Check out some of our work <a href='https://www.instagram.com/zlbstudio/'>here</a>!",
+
+    tableclock: "Here is my record player. Music has always been an essential part of my life and I truly believe that it is a gift to be able to listen to music! Check out what songs I like and songs that I've made <a href='https://soundcloud.com/iluvpromethazine'>here</a>!",
+
+    hole: "This is an endless hole. Similarly to our potential as humans it is endless. Have you realized how infinite we are? ",
+
+    dresser: "This is where I keep all my clothes. I really love fashion and think it is one of the best ways for me to express myself. If you want to see some of my outfits check out my <a href='https://www.instagram.com/envisaage/'>Instagram</a>!",   
+
+    bed: "Sleeping is one of my favorite things in this entire world. However it is like a double-edged sword in my opinion. It always feels so blissful, but at the same time I know I can't sleep forever because then I'll waste away my life. Always strive for 8 hours of sleep!",
+
+    tv: "I love to watch movies and shows here! To be more specific, I really enjoy anime. If you ever have something to recommend to me or want to talk, please reach out! You can find my favorite shows <a href='https://anilist.co/user/LukeHopkins/'>here</a>!",
+
+    lamp: "Here is my favorite lamp. Recently I have been prioritizing creating the ideal lighting for my room. I think that similarly to plants, lighting can make a huge difference in your mood.",
+
+    fridge1: "In this fridge I keep my late night snacks. Although I really do strive to maintain a healthy diet filled with fruits and vegetables, every now and then I like to indulge in some chips or ice cream. I think it's important to treat yourself every now and then.",
+
+    bookshelf1: "Literature is such an crucial part to my being. I have not only learned so much, but I genuinely enjoy reading more than almost anything in the world. Some of my favorite books are <a href='https://www.goodreads.com/book/show/2165.The_Old_Man_and_the_Sea'>The Old Man and the Sea by Ernest Hemingway</a>, <a href='https://www.goodreads.com/book/show/4982.The_Sirens_of_Titan?from_search=true&from_srp=true&qid=18OeiO4ZyI&rank=1'>The Sirens of Titans by Kurt Vonnegut</a>, and <a href='https://www.goodreads.com/book/show/54493401-project-hail-mary?from_search=true&from_srp=true&qid=wHInk7sr13&rank=1'>Project Hail Mary by Andy Weir</a>.",
+
+    bookshelf2: "On this shelf is my manga collection. Similarly to books, I also love to read manga. My favorite manga right now is definitely <a href='https://anilist.co/manga/37375/The-Climber/'>The Climber by Shinichi Sakamoto</a>, but you can check out my list <a href= 'https://anilist.co/user/LukeHopkins/mangalist'>here</a>.",
+
+    papertable: "This is my <a href='https://drive.google.com/file/d/1hnlepFQsZsBiLJGIqk9tnwX2u6pvvb0U/view?usp=drive_link'>resume</a>! I am a rising sophmore at NYU studying Computer Science. I am currently looking for internships for the summer of 2025. If you have any opportunities or just want to chat, please reach out to me through the email on the resume!",
+
+    computers: "This where I create my coding projects. Recently I have been really enjoying creating projects utilizing JavaScript and libraries with JavaScript (like this project!). You can find my full portfolio of projects <a href='https://github.com/LukeHopkinss'>here</a>.",
+
+    degree: "Here is my <a href='https://www.onyxunity.com/models/543-luke/'>modeling portfolio</a>! I've recently started modeling and it has been such an enjoyable experience! I love being apart of someone's creative process, so this has been an absolute blessing! If you're intersted in working with me, contact my agent at lindsay.feldman@onyxunity.com!",
+
+    picture: "<a href='https://drive.google.com/drive/folders/1v2zQNvQJ3G37Whn1A4fIdw5xUvX_L8UT?usp=drive_link'>This</a> is a picture of me and my friends. I love my friends so much and I am so grateful to have them in my life. I believe that friends are the family you choose and I am so lucky to have chosen the best ones!",
+
+    greentree: "I really love nature and all it has to offer, so I planted this tree here so that I'll always have a little bit of nature in my life.<a href='https://drive.google.com/drive/folders/1JDGQdnMbOpEXc1RHVxRG8x01YlJ-wBDb?usp=drive_link'>Here</a> are some of my favorite pictures in nature that I've taken!",
+
+    chest: "<a href='https://drive.google.com/drive/folders/1h2enanbYmds0oZA16EYrok3iMAs4koGN?usp=drive_link'>This</a> is where I store all my old pictures. I love looking back at old memories and seeing how much I've grown. I think it's important to reflect on your past and see how far you've come."
+
+    
+
+}
